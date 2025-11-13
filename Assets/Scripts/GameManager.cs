7207f096128506d5f41fc311c0ccccc4fd341e77
Sabi <145985537+Wassabi42@users.noreply.github.com>
2025-11-13 16:07:11 +0100

    upgrades

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,18 +3,18 @@ using UnityEngine.InputSystem;
 
 public class GameManager : MonoBehaviour
 {
+    [SerializeField] ParticleSystem teleportParticles;
+
     InputAction spaceAction;
 
     [SerializeField] Transform playerPosition;
     [SerializeField] Transform orbPosition;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Awake()
     {
         spaceAction = InputSystem.actions.FindAction("Jump");
     }
 
-    // Update is called once per frame
     void Update()
     {
         if (spaceAction.WasPerformedThisFrame())
@@ -28,5 +28,6 @@ public class GameManager : MonoBehaviour
         Vector3 tempPosition = playerPosition.position;
         playerPosition.position = orbPosition.position;
         orbPosition.position = tempPosition;
+        teleportParticles.Play();
     }
 }
